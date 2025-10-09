fbaa20a1c3d0a7112bf461c753f53c039965231c
Michal Opala <mopala@opennebula.io>
2025-10-09 16:36:56 +0200

    Add flavors and federation

--- microenv.rb
+++ microenv.rb
@@ -10,11 +10,13 @@ ONE_XMLRPC = 'http://10.2.11.40:2633/RPC2'
 ONE_AUTH   = 'oneadmin:asd'
 
 TEMPLATE     = 'ubuntu2404'
-MICROENV     = 'kvm-ssh'
+MICROENV     = 'kvm-fed'
+FLAVORS      = ENV.fetch('FLAVORS', '')
+FEDENV       = ENV.fetch('FEDENV', '')
 VERSION      = '7.0'
 ID           = 'asd12'
 DOMAIN       = 'test'
-STEM         = "#{TEMPLATE}-#{MICROENV}-#{VERSION.gsub(%[\.], %[\-])}-#{ID}"
+STEM         = [TEMPLATE, MICROENV, *FLAVORS.split(%[\+]), *VERSION.split(%[\.]), ID].reject(&:empty?).join(%[\-])
 VM_STEM      = STEM
 VNET_STEM    = "private-#{STEM}"
 VMGROUP_STEM = STEM
@@ -45,6 +47,62 @@ def combine(*args, merge_lists: false)
     ab.first
 end
 
+def collect_paths(flavors)
+    raise unless (dir = File.realpath(Dir.exist?(t = "./#{MICROENV}/") ? t : './')).end_with?(MICROENV)
+    Dir["#{dir}/*"].each_with_object({}) do |path, acc|
+        name, *tags = File.basename(path).split(%[\#])
+        next unless %w[ ansible.cfg
+                        infra.yml
+                        inventory.yml
+                        site.yml ].include?(name)
+        acc[path] = {
+            :name    => name,
+            :flavors => (x = tags.reject { |tag| tag.to_s[/^\d+$/] }).empty? ? [''] : x,
+            :fedenvs => (y = tags.select { |tag| tag.to_s[/^\d+$/] }).empty? ? [''] : y
+        }
+    end.then do |by_path|
+        # group files by flavor
+        (by_path.map { |_, v| v[:flavors] }).flatten.uniq.each_with_object({}) do |flavor, acc|
+            acc[flavor] = by_path.select { |_, v| v[:flavors].include?(flavor) }
+        end
+    end.then do |by_flavor|
+        # group files by fedenv for each flavor
+        by_flavor.each_with_object({}) do |(flavor, vv), acc|
+            (vv.map { |_, v| v[:fedenvs] }).flatten.uniq.each do |fedenv|
+                acc[flavor] ||= {}
+                acc[flavor][fedenv] = by_flavor[flavor].select { |k, v| v[:fedenvs].include?(fedenv) }
+            end
+        end
+    end.then do |by_flavor|
+        # convert path -> name mapping to name -> path + clean it up
+        by_flavor.each do |flavor, by_fedenv|
+            by_fedenv.each do |fedenv, by_path|
+                by_name = by_path.each_with_object({}) do |(path, v), acc|
+                    acc[v[:name]] = path
+                end
+                by_fedenv[fedenv].replace by_name
+            end
+        end
+        by_flavor
+    end.then do |by_flavor|
+        # pick and merge together only flavors specified by the caller
+        by_fedenv = flavors.to_s.split(%[\+]).then do |ff|
+            ff = [''] if ff.empty?
+            by_flavor.slice(*ff).then do |sliced|
+                raise unless (sliced.keys - ff).empty?
+                combine *sliced.values
+            end
+        end
+        # merge each specific fedenv with the default flavor
+        by_fedenv.keys.each do |fedenv|
+            m = combine (by_flavor.dig('', '') || {}),
+                        by_fedenv[fedenv]
+            by_fedenv[fedenv].replace m
+        end
+        by_fedenv
+    end
+end
+
 def to_one(h)
     (recurse = proc { |h|
         h.each_with_object([]) do |(k, vv), acc|
@@ -62,19 +120,14 @@ def to_one(h)
     }).call(h).join(%[\n])
 end
 
-def parse_extra_yml(b)
-    case
-    when File.exist?(path = "./#{MICROENV}/extra.yml")
-    when File.exist?(path = "./extra.yml")
-    else raise
-    end
+def parse_infra_yml(path, b)
     YAML.load ERB.new(File.read(path), :trim_mode => %[\-]).result(b)
 end
 
-def ensure_vnets(one, extra_yml)
+def ensure_vnets(one, infra_yml)
     rc = (pool = OpenNebula::VirtualNetworkPool.new(one)).info
     pp(rc).then{exit(-1)} if OpenNebula.is_error?(rc)
-    extra_yml['vnets'].each do |it|
+    infra_yml['vnets'].each do |it|
         next if pool.find { |f| f.name == it.dig('template', 'NAME') }
         x = OpenNebula::VirtualNetwork.new OpenNebula::VirtualNetwork.build_xml, one
         rc = x.allocate to_one(it.dig('template')), -1
@@ -82,7 +135,7 @@ def ensure_vnets(one, extra_yml)
     end
 end
 
-def ensure_vmgroup(one, extra_yml)
+def ensure_vmgroup(one, infra_yml)
     rc = (pool = OpenNebula::VMGroupPool.new(one)).info
     pp(rc).then{exit(-1)} if OpenNebula.is_error?(rc)
     unless pool.find { |f| f.name == VMGROUP_STEM }
@@ -95,11 +148,11 @@ def ensure_vmgroup(one, extra_yml)
     end
 end
 
-def ensure_templates(one, extra_yml)
+def ensure_templates(one, infra_yml)
     rc = (pool = OpenNebula::TemplatePool.new(one)).info
     pp(rc).then{exit(-1)} if OpenNebula.is_error?(rc)
     exit(-1) unless (b = pool.find { |f| f.name == TEMPLATE })
-    extra_yml['vms'].each do |it|
+    infra_yml['vms'].each do |it|
         next if pool.find { |f| f.name == it.dig('template', 'NAME') }
         id = b.clone it.dig('template', 'NAME'), true
         pp(id).then{exit(-1)} if OpenNebula.is_error?(id)
@@ -115,12 +168,12 @@ def ensure_templates(one, extra_yml)
     end
 end
 
-def ensure_vms(one, extra_yml)
+def ensure_vms(one, infra_yml)
     rc = (pool1 = OpenNebula::VirtualMachinePool.new(one)).info
     pp(rc).then{exit(-1)} if OpenNebula.is_error?(rc)
     rc = (pool2 = OpenNebula::TemplatePool.new(one)).info
     pp(rc).then{exit(-1)} if OpenNebula.is_error?(rc)
-    extra_yml['vms'].each do |it|
+    infra_yml['vms'].each do |it|
         next if pool1.find { |f| f.name == it.dig('template', 'NAME') }
         exit(-1) unless (x = pool2.find { |f| f.name == it.dig('template', 'NAME') })
         rc = x.instantiate it.dig('template', 'NAME')
@@ -128,7 +181,7 @@ def ensure_vms(one, extra_yml)
     end
 end
 
-def extract_groups(inventory_yml)
+def build_groups(inventory_yml)
     inventory_yml.each_with_object({}) do |(g, v), acc|
         acc[g] ||= {}
         acc[g]['hosts']    = v['hosts'].keys unless v['hosts'].nil?
@@ -147,7 +200,7 @@ def extract_groups(inventory_yml)
     end
 end
 
-def extract_hostvars(inventory_yml)
+def build_hostvars(inventory_yml)
     (recurse = proc { |node, vars, acc|
         node&.dig('children').to_a.each do |g|
             m = combine vars.to_h,
@@ -166,43 +219,62 @@ def extract_hostvars(inventory_yml)
 end
 
 def build_inventory(inventory_yml)
-    groups = extract_groups(inventory_yml)
+    groups = build_groups(inventory_yml)
 
     inventory_yml['all']['children'] = groups['all']['children']
 
-    hostvars = extract_hostvars(inventory_yml)
+    hostvars = build_hostvars(inventory_yml)
 
     combine ({ '_meta' => { 'hostvars' => hostvars } }), groups
 end
 
-def parse_inventory_yml(b)
-    case
-    when File.exist?(path = "./#{MICROENV}/inventory.yml")
-    when File.exist?(path = "./inventory.yml")
-    else raise
-    end
+def parse_inventory_yml(path, b)
     YAML.load ERB.new(File.read(path), :trim_mode => %[\-]).result(b)
 end
 
-def render_inventory(one, extra_yml)
+def render_inventory(one, paths)
     rc = (pool = OpenNebula::VirtualMachinePool.new(one)).info
     pp(rc).then{exit(-1)} if OpenNebula.is_error?(rc)
-    extra_yml['vms'].each_with_object([]) do |it, acc|
+    parse_infra_yml(paths[FEDENV]['infra.yml'], binding)['vms'].each_with_object([]) do |it, acc|
         next unless (vm = pool.find { |f| f.name == it.dig('template', 'NAME') })
         h = vm.to_hash['VM']
         acc << {
             'inventory_hostname' => h.dig('NAME'),
-            'ansible_host'       => h.dig('TEMPLATE', 'NIC', 0, 'IP')
+            'ansible_host'       => [h.dig('TEMPLATE', 'NIC')].flatten.dig(0, 'IP')
         }
     end.then do |vms|
-        inventory_yml = parse_inventory_yml(binding)
-        JSON.dump(build_inventory(inventory_yml))
+        JSON.dump build_inventory(
+            parse_inventory_yml(paths[FEDENV]['inventory.yml'], binding)
+        )
     end
 end
 
-def run_ansible
-    Dir.chdir "./#{MICROENV}/"
-    exec 'ansible-playbook', '-v', 'site.yml'
+def run_ansible(paths, fedenv)
+    Dir.chdir("./#{MICROENV}/") do
+        env = {
+            'FLAVORS'        => FLAVORS,
+            'FEDENV'         => fedenv,
+            'ANSIBLE_CONFIG' => paths[fedenv]['ansible.cfg']
+        }
+        raise unless system env, 'ansible-playbook', '-v', paths[fedenv]['site.yml']
+    end
+end
+
+def microenv_apply(one, paths)
+    case paths.length
+    when 0 then raise
+    when 1
+        [FEDENV]
+    else
+        (paths.keys - ['']).map(&:to_i).sort.map(&:to_s)
+    end.each do |fedenv|
+        infra_yml = parse_infra_yml paths[fedenv]['infra.yml'], binding
+        ensure_vnets one, infra_yml
+        ensure_vmgroup one, infra_yml
+        ensure_templates one, infra_yml
+        ensure_vms one, infra_yml
+        run_ansible paths, fedenv
+    end
 end
 
 if caller.empty?
@@ -213,16 +285,13 @@ if caller.empty?
         opt.on('--list')          { options[:list] = true }
     end.parse!
 
-    args = OpenNebula::Client.new(ONE_AUTH, ONE_XMLRPC, :sync => true), parse_extra_yml(binding)
+    paths = collect_paths FLAVORS
+
+    one = OpenNebula::Client.new ONE_AUTH, ONE_XMLRPC, :sync => true
 
     case
-    when options[:apply]
-        ensure_vnets(*args)
-        ensure_vmgroup(*args)
-        ensure_templates(*args)
-        ensure_vms(*args)
-        run_ansible
-    when options[:list]
-        puts render_inventory(*args)
+    when options[:apply] then microenv_apply(one, paths)
+    when options[:list]  then puts render_inventory(one, paths)
+    else raise
     end
 end
